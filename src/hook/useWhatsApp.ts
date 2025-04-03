31e84ef703244f59546b0022fdb6de21627e7e24
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2025-04-02 21:41:38 -0300

    Update useWhatsApp.ts

--- src/hook/useWhatsApp.ts
+++ src/hook/useWhatsApp.ts
@@ -1,7 +1,7 @@
 export function useWhatsApp() {
-    const sendMessage = (title: string, fatias: string) => {
+    const sendMessage = (title: string) => {
         const phoneNumber = "5514998039544";
-        const message = `Olá! Gostaria de saber mais sobre o *${title}* que *serve até ${fatias}*. Poderia me passar mais informações?`;
+        const message = `Olá! Gostaria de encomendar o *${title}*. Poderia me passar mais informações?`;
         const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
         window.open(url, "_blank");
     };
